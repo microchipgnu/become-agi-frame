2541f0463a483880c2f48fccd2ef529411e15bd1
Markeljan Sokoli <Markeljan19@gmail.com>
2024-03-23 14:59:45 -0400

    Revert "remove unnecessary route"
    This reverts commit 85d2c8bb083e3b75ffdd4c52e3c3c41d034fc4db.

---
+++ app/share/route.tsx
@@ -0,0 +1,31 @@
+import { createFrames, Button } from "frames.js/next";
+
+const frames = createFrames();
+
+export const handleShareRequest = frames(async (ctx) => {
+  console.log("share ctx.message", ctx?.message);
+
+  const { requesterFid } = ctx?.message || {};
+  return {
+    image: (
+      <div tw="flex flex-col w-full h-full bg-slate-700 text-white justify-center items-center">
+        <p>User {requesterFid}</p>
+        <p>SHARE PAGE IMAGE</p>
+      </div>
+    ),
+    imageOptions: {
+      aspectRatio: "1:1",
+    },
+    buttons: [
+      <Button key="b1" action="post" target="share/result?injectNoise=true">
+        INJECT NOISE
+      </Button>,
+      <Button key="b2" action="post" target="share/result?injectNoise=false">
+        MAINTAIN INTEGRITY
+      </Button>,
+    ],
+  };
+});
+
+export const GET = handleShareRequest;
+export const POST = handleShareRequest;
