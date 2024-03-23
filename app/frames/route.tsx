93ced6a10aef15a408086d43f6b4ed490672275a
Luís Freitas <microchipgnu@gmail.com>
2024-03-23 15:39:28 +0000

    wip

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -3,13 +3,30 @@ import { createFrames, Button } from "frames.js/next";
 const frames = createFrames();
 
 const handleRequest = frames(async (ctx) => {
+  console.log(ctx);
+
+  if (!ctx.message) {
+    return {
+      image: (
+        <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
+          COVER
+        </div>
+      ),
+      buttons: [<Button action="post">LAUNCH</Button>],
+    };
+  }
+
   return {
     image: (
       <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-        COVER
+        MAIN APP
       </div>
     ),
-    buttons: [<Button action="post">LAUNCH</Button>],
+    buttons: [
+      <Button action="post">BENCHMARK</Button>,
+      <Button action="post">TRAIN</Button>,
+      <Button action="post">DISTRIBUTE</Button>,
+    ],
   };
 });
 
